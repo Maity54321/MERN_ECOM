94ccc295b9f32f5b7fc68205cec5923c8217d17c
Maity54321 <115551421+Maity54321@users.noreply.github.com>
2023-07-12 17:38:25 +0530

    Update paymentController.js

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -44,7 +44,7 @@ exports.paymentVerification = async (req, res) => {
     });
     // console.log(paymentDetails);
     res.redirect(
-      `https://mern-ecom-frontend.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
+      `https//localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({ success: false });
