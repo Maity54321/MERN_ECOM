773dee6733e953506de9e6707ef6a1be2b8f0872
maityjeet <maityjeet786@gmail.com>
2025-06-11 23:17:04 +0530

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.js', // or './src/index.js'
+  target: 'node',
+  mode: 'development', // or 'production' for deployment
+  externals: [nodeExternals()], // ignores node_modules
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // add '.ts' if using TypeScript
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // change to /\.js$/ if using JS
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
